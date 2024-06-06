a931457753c100e8b5345da491426b436cea112a
Nakidai <plaza521@inbox.ru>
2024-06-06 18:55:07 +0300

    Add code

---
+++ README.md
@@ -0,0 +1,7 @@
+FiltShopBot
+--
+Bot for filtering messages with invites to shops
+
+Usage
+--
+Install it and start bot with token as first argument
---
+++ src/filtshopbot/main.py
@@ -0,0 +1,44 @@
+import argparse
+import re
+
+import discord
+
+
+class Bot(discord.Client):
+    def __init__(self, banwords: list[str]) -> None:
+        self.banwords: list[str] = banwords
+        self.filter = re.compile(
+            r"(?:https?://)?discord(?:(?:app)?\.com/invite|\.gg)/?[a-zA-Z0-9]+/?"
+        )
+
+        intents = discord.Intents.default()
+        intents.message_content = True
+        super(Bot, self).__init__(intents=intents)
+
+    async def on_message(
+        self,
+
+        message: discord.Message
+    ) -> None:
+        for invite_link in self.filter.findall(message.content):
+            invite = await self.fetch_invite(invite_link)
+            if invite.guild is None:
+                continue
+            if isinstance(invite.guild, discord.PartialInviteGuild):
+                info: str = f"{invite.guild.name} {invite.guild.description}"
+            print(f"{message.content}: {info}")
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        prog="filtshopbot",
+        description="bot for filtering messages with invites to shops"
+    )
+    parser.add_argument(
+        "token",
+        help="discord bot token"
+    )
+
+    args = parser.parse_args()
+    bot = Bot([])
+    bot.run(args.token)
